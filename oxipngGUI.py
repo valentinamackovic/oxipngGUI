9c53067d6ec429131bad6f390fd25b434e8ade66
Valentina Mackovic <mackovicvalentina@gmail.com>
2020-04-11 22:45:23 +0200

    Set options based on input

--- oxipngGUI.py
+++ oxipngGUI.py
@@ -4,6 +4,12 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QDialog, QRubberBand,
                              QCheckBox, QHBoxLayout, QGridLayout, QGroupBox, QPushButton, QFileDialog, QLabel, QSpacerItem, QSpinBox, QButtonGroup, QRadioButton)
 
+options = {
+    "optimization": 2,
+    "interlacing": 1,
+    "strip": "safe"
+}
+
 
 class MainWindow(QDialog):
     def __init__(self, parent=None):
@@ -13,12 +19,14 @@ class MainWindow(QDialog):
         self.setGeometry(400, 300, 800, 600)
 
         self.cbInterlacing = QCheckBox('Interlacing')
+        self.cbInterlacing.setChecked(True)
 
         optimizationLabel = QLabel(self)
         optimizationLabel.setText("Optimization level: ")
         self.optimizationSpinBox = QSpinBox()
         self.optimizationSpinBox.setMaximum(6)
         self.optimizationSpinBox.setMinimum(0)
+        self.optimizationSpinBox.setValue(2)
         optimizationLabelLayout = QHBoxLayout()
         optimizationLabelLayout.addWidget(optimizationLabel)
         optimizationLabelLayout.addWidget(self.optimizationSpinBox)
@@ -28,6 +36,7 @@ class MainWindow(QDialog):
         self.rbGroupRemoveMetadata = QButtonGroup()
         self.rbRemoveMetadataAll = QRadioButton("All")
         self.rbRemoveMetadataSafe = QRadioButton("Safe")
+        self.rbRemoveMetadataSafe.setChecked(True)
         self.rbGroupRemoveMetadata.addButton(self.rbRemoveMetadataAll)
         self.rbGroupRemoveMetadata.addButton(self.rbRemoveMetadataSafe)
         removeMetadataLayout = QHBoxLayout()
@@ -51,6 +60,12 @@ class MainWindow(QDialog):
         mainLayout.setColumnStretch(1, 1)
 
         self.setLayout(mainLayout)
+        self.setOptions()
+
+    def setOptions(self):
+        options['interlacing'] = 1 if self.cbInterlacing.isChecked() else 0
+        options['optimization'] = self.optimizationSpinBox.value()
+        options['strip'] = 'safe' if self.rbRemoveMetadataSafe.isChecked() else 'all'
 
 
 if __name__ == '__main__':
